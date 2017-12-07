f559f5dc237189c0714be26b473638444d716655
Rafael Carvalho Caetano <rapha.pse@outlook.com>
2017-12-07 18:59:37 -0200

    Adicionando projeto final IFSP - CRUD completo com navegação entre activity

---
+++ ProjetoFinal/app/src/main/java/projetofinal/com/br/projetofinal/MainActivity.java
@@ -0,0 +1,21 @@
+package projetofinal.com.br.projetofinal;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
---
+++ ProjetoFinal/app/src/main/java/projetofinal/com/br/projetofinal/PessoaFisica.java
@@ -0,0 +1,16 @@
+package projetofinal.com.br.projetofinal;
+
+import android.app.Activity;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+/**
+ * Created by rafaelcarvalho on 07/12/2017.
+ */
+
+public class PessoaFisica extends Activity implements OnClickListener{
+    @Override
+    public void onClick(View view) {
+
+    }
+}
