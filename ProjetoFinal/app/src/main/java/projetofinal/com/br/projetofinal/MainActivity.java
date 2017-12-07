55ab73bad7e28f5ef04e241a13ec406ed2a33112
Rafael Carvalho Caetano <rapha.pse@outlook.com>
2017-12-07 20:59:31 -0200

    Adicionando projeto final IFSP - CRUD completo com navegação entre activity

--- ProjetoFinal/app/src/main/java/projetofinal/com/br/projetofinal/MainActivity.java
+++ ProjetoFinal/app/src/main/java/projetofinal/com/br/projetofinal/MainActivity.java
@@ -1,21 +1,31 @@
 package projetofinal.com.br.projetofinal;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity implements OnClickListener {
+
+    private Button cadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cadastrar = (Button) findViewById(R.id.btnCadastrar);
+        cadastrar.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
 
+        if(view == cadastrar){
+            startActivity(new Intent(MainActivity.this, PessoaFisica.class));
+        }
     }
 }
--- ProjetoFinal/app/src/main/java/projetofinal/com/br/projetofinal/PessoaFisica.java
+++ ProjetoFinal/app/src/main/java/projetofinal/com/br/projetofinal/PessoaFisica.java
@@ -1,6 +1,8 @@
 package projetofinal.com.br.projetofinal;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.View;
 import android.view.View.OnClickListener;
 
@@ -9,6 +11,13 @@ import android.view.View.OnClickListener;
  */
 
 public class PessoaFisica extends Activity implements OnClickListener{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cadastro);
+    }
+
     @Override
     public void onClick(View view) {
 
